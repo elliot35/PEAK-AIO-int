4886cb3e9b8c0d3f06524dce950f0454d1d4c8e9
Elliot He <41864818+elliot35@users.noreply.github.com>
2026-03-06 23:55:40 +1100

    chore: bump version

--- PEAK-AIO/Main.cs
+++ PEAK-AIO/Main.cs
@@ -13,7 +13,7 @@ using Photon.Pun;
 using System.Collections.Generic;
 
 [BepInDependency(DearImGuiInjection.Metadata.GUID)]
-[BepInPlugin("com.onigremlin.peakaio", "PEAK AIO Mod", "1.0.10")]
+[BepInPlugin("com.onigremlin.peakaio", "PEAK AIO Mod", "1.0.12")]
 
 public class PeakMod : BaseUnityPlugin
 {
